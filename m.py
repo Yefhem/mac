18664a4edc35ecdca445c6e247ba598b2ef26bd3
layef <sametcelikbusiness@gmail.com>
2021-09-20 01:37:45 +0300

    finish today

--- m.py
+++ m.py
@@ -5,7 +5,7 @@ import subprocess, optparse, re, sys, os, time
 class MacChanger():
 	def __init__(self):
 		os.system('clear')
-		print("**Welcome**")
+		print("  ** MacChanger **  \n")
 		
 	def input(self):
 		usage = "usage: %prog [options] -i eth0 -m 00:EE:SS:28:10:98"
@@ -15,35 +15,29 @@ class MacChanger():
 		(userInput, arguments) = parseObject.parse_args()  
 			
 		if not userInput.interface or not userInput.macAdd:
-			sys.stderr.write("[-]Error!Eksik parametre!!\n")
+			sys.stderr.write("> Error! Missing parameter!!\n")
 			sys.exit()
-		else:
-			return userInput
+		return userInput
 			
 	def change(self,interface,macAdd):
 		subprocess.call(["ifconfig",interface,"down"])
 		subprocess.call(["ifconfig",interface,"hw","ether",macAdd])
 		subprocess.call(["ifconfig",interface,"up"])
 		
-	def mac_getir(self,interface):
+	def get(self,interface):
 		ifconfig = subprocess.check_output(["ifconfig",interface])
-		gelen_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
+		mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
 	
-		return gelen_mac.group(0)
+		return mac.group(0)
 		
+macChanger = MacChanger()
+options = macChanger.input()
+oldMac = macChanger.get(options.interface)
+macChanger.change(options.interface, options.macAdd)
+newMac = macChanger.get(options.interface)
 
-mac_changer = MacChanger()
-options = mac_changer.input()
-
-eski_mac_adres = mac_changer.mac_getir(options.interface)
-
-mac_changer.change(options.interface, options.macAdd)
-
-yeni_mac_adres = mac_changer.mac_getir(options.interface)
-
-
-if yeni_mac_adres == options.macAdd:
+if newMac == options.macAdd:
 	time.sleep(0.5)
-	print("> Change successful! \n\n> Old Mac: {} \n> New Mac: {}".format(eski_mac_adres,yeni_mac_adres))
+	print("> Change successful! \n\n> Old Mac: {} \n> New Mac: {}".format(oldMac,newMac))
 else:
-	print("[-]MAC not changed!")
+	print("> MAC not changed!")
