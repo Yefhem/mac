b448224d740dcdbb455e1e9ace96d2555ffaa15f
layef <sametcelikbusiness@gmail.com>
2021-09-19 23:34:56 +0300

    edited

--- m.py
+++ m.py
@@ -1,44 +1,49 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import subprocess, optparse, re, time
+import subprocess, optparse, re, sys, os, time
 
-def input():
-	usage = "usage: %prog [options] -i <interface> -m <mac_address>"
-	parseObject = optparse.OptionParser(usage)
-	parseObject.add_option("-i","--interface",dest="interface",help="Please enter the interface you want to change!")
-	parseObject.add_option("-m","--mac",dest="macAdd",help="Please enter a MAC address!")
-
-	return parseObject.parse_args()  # Bir tuple döndürüyor
-
-def change(userInterface,userMacAdd):
-	subprocess.call(["ifconfig",userInterface,"down"])
-	subprocess.call(["ifconfig",userInterface,"hw","ether",userMacAdd])
-	subprocess.call(["ifconfig",userInterface,"up"])
-
-def oldMac(interface):
-	ifconfig = subprocess.check_output(["ifconfig",interface])
-	oldMac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
+class MacChanger():
+	def __init__(self):
+		os.system('clear')
+		print("**Welcome**")
+		
+	def input(self):
+		usage = "usage: %prog [options] -i eth0 -m 00:EE:SS:28:10:98"
+		parseObject = optparse.OptionParser(usage)
+		parseObject.add_option("-i","--interface",dest="interface",help="Please enter the interface you want to change!")
+		parseObject.add_option("-m","--mac",dest="macAdd",help="Please enter a MAC address!")
+		(userInput, arguments) = parseObject.parse_args()  
+			
+		if not userInput.interface or not userInput.macAdd:
+			sys.stderr.write("[-]Error!Eksik parametre!!\n")
+			sys.exit()
+		else:
+			return userInput
+			
+	def change(self,interface,macAdd):
+		subprocess.call(["ifconfig",interface,"down"])
+		subprocess.call(["ifconfig",interface,"hw","ether",macAdd])
+		subprocess.call(["ifconfig",interface,"up"])
+		
+	def mac_getir(self,interface):
+		ifconfig = subprocess.check_output(["ifconfig",interface])
+		gelen_mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
 	
-	return oldMac.group(0)
+		return gelen_mac.group(0)
+		
 
-def newMac(interface):
-	ifconfig = subprocess.check_output(["ifconfig",interface])
-	newMac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
-	
-	if newMac: 
-		return newMac.group(0)
-	else:
-		return None
+mac_changer = MacChanger()
+options = mac_changer.input()
 
-(userInput, arguments)= input()
-oldmac = oldMac(userInput.interface)
-change(userInput.interface, userInput.macAdd)
-newmac = newMac(userInput.interface)
+eski_mac_adres = mac_changer.mac_getir(options.interface)
+
+mac_changer.change(options.interface, options.macAdd)
+
+yeni_mac_adres = mac_changer.mac_getir(options.interface)
 
 
-if newmac == userInput.macAdd:
-	time.sleep(2)
-	print("[+] Change successful! \n\n[+]Old Mac: {} \n[+]New Mac: {}".format(oldmac,newmac))
+if yeni_mac_adres == options.macAdd:
+	time.sleep(0.5)
+	print("> Change successful! \n\n> Old Mac: {} \n> New Mac: {}".format(eski_mac_adres,yeni_mac_adres))
 else:
-	print("[-] MAC not chnaged!")
-	
+	print("[-]MAC not changed!")
