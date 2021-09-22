c4e33b2911cfc1ab57966e8b713e82725aa61ad3
layef <sametcelikbusiness@gmail.com>
2021-09-22 12:02:43 +0300

    v1.0

--- m.py
+++ m.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 
-import subprocess, optparse, re, sys, os, time
+import subprocess, argparse, re, sys, os, time
+from generate_mac import generate_mac
 
-class MacChanger():
+class Mac_changer():
 	def __init__(self):
+		pass
+		
+	def clear(self):
 		os.system('clear')
-		print("  ** MacChanger **  \nsss")
+		print(">>>> MacChanger <<<<\n")
 		
 	def input(self):
-		usage = "usage: %prog [options] -i eth0 -m 00:EE:SS:28:10:98"
-		parseObject = optparse.OptionParser(usage)
-		parseObject.add_option("-i","--interface",dest="interface",help="Please enter the interface you want to change!")
-		parseObject.add_option("-m","--mac",dest="macAdd",help="Please enter a MAC address!")
-		(userInput, arguments) = parseObject.parse_args()  
+		usage = "macChanger.py [options] \n[-i eth0 -m 00:EE:SS:28:10:98] \n[--interface eth0 --mac 00:EE:SS:28:10:98] \n[-i eth0 -r 15] \n[--interface eth0 --random 15]"
+		parseObject = argparse.ArgumentParser(usage)
+		parseObject.add_argument("-i","--interface",dest="interface",help="Please enter the interface you want to change!")
+		parseObject.add_argument("-m","--mac",dest="mac_add",help="Please enter a MAC address!")
+		parseObject.add_argument("-r","--random",dest="random_mac",type=int,help="Please specify the duration(second)!")
+		args = parseObject.parse_args()
 			
-		if not userInput.interface or not userInput.macAdd:
-			sys.stderr.write("> Error! Missing parameter!!\n")
-			sys.exit()
-		return userInput
+		if not len(sys.argv) == 5:
+			self.error()
+		return args
 			
-	def change(self,interface,macAdd):
+	def change(self,interface,mac_add):
 		subprocess.call(["ifconfig",interface,"down"])
-		subprocess.call(["ifconfig",interface,"hw","ether",macAdd])
+		subprocess.call(["ifconfig",interface,"hw","ether",mac_add])
 		subprocess.call(["ifconfig",interface,"up"])
 		
 	def get(self,interface):
@@ -29,15 +33,35 @@ class MacChanger():
 		mac = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig))
 	
 		return mac.group(0)
+	
+	def show(self,old,new):
+		print("> Change successful! \n\n> Old Mac: {} \n> New Mac: {}".format(old,new))
 		
-macChanger = MacChanger()
-options = macChanger.input()
-oldMac = macChanger.get(options.interface)
-macChanger.change(options.interface, options.macAdd)
-newMac = macChanger.get(options.interface)
+	def error(self):
+		sys.stderr.write("> Error! Missing or incorrect parameter!!\n")
+		sys.exit()
+	
+mac_changer = Mac_changer()
+options = mac_changer.input()
 
-if newMac == options.macAdd:
-	time.sleep(0.5)
-	print("> Change successful! \n\n> Old Mac: {} \n> New Mac: {}".format(oldMac,newMac))
-else:
-	print("> MAC not changed!")
+if options.random_mac and options.interface:
+	while True:
+		mac_changer.clear()
+		old_mac = mac_changer.get(options.interface)
+		new_random_mac = generate_mac.total_random()
+		mac_changer.change(options.interface,new_random_mac)
+		mac_changer.show(old_mac,new_random_mac)
+		time.sleep(options.random_mac)	
+					
+if options.interface and options.mac_add:
+	if re.match("[0-9a-f]{2}([-:]?)[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", options.mac_add.lower()):
+		mac_changer.clear()
+		old_mac = mac_changer.get(options.interface)
+		mac_changer.change(options.interface,options.mac_add)
+		new_mac = mac_changer.get(options.interface)
+		if options.mac_add == new_mac:
+			mac_changer.show(old_mac,new_mac)
+		else:
+			print("> MAC not changed!")
+	else:
+		mac_changer.error()
